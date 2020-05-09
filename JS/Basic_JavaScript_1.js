96418b4633f24fd6a92fb8ccd3762d10a6e86033
Justin P <jadamsperry@gmail.com>
2020-05-09 08:43:01 -0600

    Basic JS folder upload
    this is my first submission for JS course

---
+++ JS/Basic_JavaScript_1.js
@@ -0,0 +1,11 @@
+var myFamily = "The Perry's", dad = "Justin", mom = "Jenny", dog = "Louie";
+var myFamily = myFamily.fontcolor("blue");
+var dad = dad.fontcolor("red");
+var mom = mom.fontcolor("#00ff00");
+var dog = dog.fontcolor("red");
+
+document.write(dad + " " + mom);
+
+window.alert(4 + 6);
+
+
